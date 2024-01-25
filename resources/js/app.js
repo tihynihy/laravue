2ecc3c30d8c4f6dd075069062075560bdab2f2bc
Nihad <tihynihy@gmail.com>
2024-01-25 22:03:17 +0100

    Basic SPA Setup | Initial

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./components/App.vue";
+
+const app = createApp();
+
+app.component("App", App);
+
+app.mount("#app");
---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Hello from App vue 
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
