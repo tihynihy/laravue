e23e660dd16ef4189235292cc222868886306a52
Nihad Jusović <76163793+tihynihy@users.noreply.github.com>
2024-01-29 22:37:38 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite('resources/js/app.js')
-        <title>Laravel</title>
+        <title>Title placeholder</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
